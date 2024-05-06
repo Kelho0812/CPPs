486ddc9d4d71629de466c34a6e529a21635575a5
Jorge Costa <Kelho88@pm.me>
2024-05-06 10:20:50 +0100

    chore: Remove redundant print_all_contacts method in Contact class

--- CPP00/ex01/src/Contact.cpp
+++ CPP00/ex01/src/Contact.cpp
@@ -46,15 +46,6 @@ void Contact::print_contact(void)
 	std::cout << "Darkest Secret: " << this->darkest_secret << std::endl;
 }
 
-void Contact::print_all_contacts(void)
-{
-	std::cout << "First Name: " << this->first_name << std::endl;
-	std::cout << "Last Name: " << this->last_name << std::endl;
-	std::cout << "Nickname: " << this->nickname << std::endl;
-	std::cout << "Phone Number: " << this->phone_number << std::endl;
-	std::cout << "Darkest Secret: " << this->darkest_secret << std::endl;
-}
-
 Contact::~Contact()
 {
 	// Nothing to clean up

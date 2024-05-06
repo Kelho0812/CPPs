8117b207165a2bdc705433fff16ceb6c0bfc9c03
Jorge Costa <Kelho88@pm.me>
2024-05-06 10:20:57 +0100

    chore: Update main.cpp to clear console before printing menu and display phonebook on SEARCH command

--- CPP00/ex01/src/main.cpp
+++ CPP00/ex01/src/main.cpp
@@ -1,30 +1,31 @@
 #include "../include/PhoneBook.hpp"
 #include <cstdlib>
+#include <iomanip>
 #include <iostream>
+#include <string>
 
 int main(void)
 {
 	PhoneBook *phonebook = new PhoneBook;
 	std::string command;
-	phonebook->print_menu();
+	system("clear");
+	phonebook->print_menu(true);
 	while (std::cin >> command)
 	{
-		if (command == "EXIT")
+		if (command == "EXIT" ||  command == "exit")
 		{
 			std::cout << "Goodbye" << std::endl;
 			break;
 		}
-		else if (command == "ADD")
+		else if (command == "ADD" || command == "add")
 		{
 			phonebook->add_contact_to_phonebook();
 		}
-		else if (command == "SEARCH")
+		else if (command == "SEARCH" || command == "search")
 		{
+			phonebook->display_phonebook();
 		}
-		else
-		{
-			phonebook->print_menu();
-		}
+		phonebook->print_menu();
 	}
 	delete phonebook;
 	return (0);

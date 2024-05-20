0276b8d40489c4ff4a02a63d68ce63c2efedd4c5
Jorge Costa <Kelho88@pm.me>
2024-05-20 12:10:48 +0100

    chore: Implement copy constructor and assignment operator for Fixed class

---
+++ cpp02/ex00/src/Fixed.cpp
@@ -0,0 +1,18 @@
+#include "../includes/Fixed.hpp"
+
+Fixed::Fixed()
+{
+	integer = 0;
+}
+Fixed::~Fixed()
+{
+}
+Fixed::Fixed(const Fixed &fixed)
+{
+}
+Fixed &Fixed::operator=(const Fixed &fixed)
+{
+	// Your code here
+
+	return *this;
+}

39a01bc1f76192692f9331170739bd361b2b96c1
Jorge Costa <Kelho88@pm.me>
2024-05-21 12:20:26 +0100

    chore: Refactor main.cpp for cpp02/ex00

--- cpp02/ex00/src/main.cpp
+++ cpp02/ex00/src/main.cpp
@@ -1,14 +1,13 @@
 #include "../includes/Fixed.hpp"
 #include <iostream>
 
-
 int main(int argc, char const *argv[])
 {
-	(void) argc;
-	(void) argv;
+	(void)argc;
+	(void)argv;
 
 	Fixed a;
-	Fixed b( a );
+	Fixed b(a);
 	Fixed c;
 	c = b;
 	std::cout << a.getRawBits() << std::endl;

82b39ee609a5b8c569c06628f9937e05986146fc
Jorge Costa <Kelho88@pm.me>
2024-06-06 11:04:16 +0100

    refactor: Update ClapTrap class member variables to use underscore prefix
    The code changes in ClapTrap.hpp and ClapTrap.cpp update the member variables of the ClapTrap class to use the underscore prefix convention. This refactor enhances the code by improving the clarity and consistency of variable naming within the class.

--- cpp03/ex01/src/Log.cpp
+++ cpp03/ex01/src/Log.cpp
@@ -2,12 +2,13 @@
 
 void Log::noEnergyLog(string type, string name)
 {
-	cout << type << " " << name << " has no energy left." << endl;
+	cout << "🪫   " << type << " " << name << " has no energy left." << endl;
 }
 
 void Log::constructorLog(string type, string name)
 {
-	cout << "🤖   " << type << " " << name << " was constructed." << endl;
+	cout << "🤖   " << type << " " << name << " was constructed."
+		 << endl;
 }
 
 void Log::destructorLog(string type, string name)
@@ -15,25 +16,31 @@ void Log::destructorLog(string type, string name)
 	cout << "💀   " << type << " " << name << " was destroyed." << endl;
 }
 
-void Log::attackLog(string type, string name, string target, int attackDamage)
+void Log::attackLog(string type, string name, string target, int attackDamage, int energyPoints)
 {
-	cout << "🗡️    " << type << " " << name << " attacks " << target << " causing " << attackDamage
-		 << " points of damage!" << endl;
+	cout << "🗡️    " << type << " " << name << " attacks " << target
+		 << " causing " << attackDamage << " points of damage!" << endl;
+	cout << "⚡   ClapTrap " << name << " has now "
+		 << energyPoints << " energy points." << endl;
 }
 
 void Log::beRepairedLog(string type, string name, unsigned int amount, int hitPoint, int energyPoints)
 {
-	cout << "❤️‍🩹  " << type << " " << name << " was repaired for " << amount << " hit points." << endl;
-	cout << "❤️‍🩹  ClapTrap " << name << " has now " << hitPoint << " hit points left." << endl;
-	cout << "❤️‍🩹  ClapTrap " << name << " has now " << energyPoints << " energy points." << endl;
+	cout << "❤️‍🩹  " << type << " " << name << " was repaired for "
+		 << amount << " hit points." << endl;
+	cout << "❤️‍🩹  ClapTrap " << name << " has now "
+		 << hitPoint << " hit points left." << endl;
+	cout << "⚡   ClapTrap " << name << " has now "
+		 << energyPoints << " energy points." << endl;
 }
 
-void Log::takeDamageLog(string type, string name, unsigned int amount, int hitPoint)
+void Log::takeDamageLog(string type, string name,unsigned int amount, int hitPoint)
 {
-	cout << "💔   " << type << " " << name << " takes " << amount << " points of damage !" << endl;
-	cout << "💔   " << type << " " << name << " has now " << hitPoint << " hit points left." << endl;
+	cout << "💔   " << type << " " << name << " takes " << amount
+		 << " points of damage !" << endl;
+	cout << "💔   " << type << " " << name << " has now " << hitPoint
+		 << " hit points left." << endl;
 }
-
 void Log::guardGateLog(string type, string name)
 {
 	cout << "⛨    " << type << " " << name << " is now on Gate Keeper mode." << endl;

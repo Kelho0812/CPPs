418b20ceb6a81a6a0ff664adf987253c56ed70ca
Jorge Costa <Kelho88@pm.me>
2024-09-17 13:36:49 +0100

    added all Bureaucrat functions

--- cpp05/ex00/src/Bureaucrat.cpp
+++ cpp05/ex00/src/Bureaucrat.cpp
@@ -10,3 +10,87 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(void) : _name("default"), _grade(150)
+{
+	std::cout << "Bureaucrat Default Constructor called.\nName: " << this->getName() <<
+		"\nGrade:" << this->getGrade() << std::endl;
+}
+
+Bureaucrat::Bureaucrat(std::string const &name, int grade)
+{
+	this->_name = name;
+	if (grade < 1)
+	{
+		throw(Bureaucrat::GradeTooHighException());
+	}
+	else if (grade > 150)
+	{
+		throw(Bureaucrat::GradeTooLowException());
+	}
+	this->_grade = grade;
+	std::cout << "Bureaucrat Custom Constructor called.\nName: " << this->getName() <<
+		"\nGrade:" << this->getGrade() << std::endl;
+}
+
+Bureaucrat::Bureaucrat(Bureaucrat const &copy)
+{
+	*this = copy;
+}
+
+const Bureaucrat &Bureaucrat::operator=(const Bureaucrat &copy)
+{
+	if (this != &copy) {
+		this->_name = copy._name;
+		this->_grade = copy._grade;
+	}
+	return *this;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "Bureaucrat Default Destructor called.\nName: " << this->getName() <<
+		"\nGrade:" << this->getGrade() << std::endl;
+}
+
+const std::string &Bureaucrat::getName() const
+{
+	return this->_name;
+}
+
+const int &Bureaucrat::getGrade() const
+{
+	return this->_grade;
+}
+
+void Bureaucrat::gradeDown()
+{
+	if (this->_grade == 150)
+	{
+		throw(Bureaucrat::GradeTooLowException());
+	}
+}
+
+void Bureaucrat::gradeUp()
+{
+	if (this->_grade == 1)
+	{
+		throw(Bureaucrat::GradeTooHighException());
+	}
+}
+
+char const *Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return ("The grade is too high");
+}
+
+char const *Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("The grade is too low");
+}
+
+std::ostream	&operator<<(std::ostream &str, Bureaucrat const &bureaucrat)
+{
+	return (str << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << ".");
+}
---
+++ cpp05/ex00/src/main.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jorteixe  <jorteixe@student.42.fr   >      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/12 10:39:06 by jorteixe          #+#    #+#             */
+/*   Updated: 2024/09/12 10:39:40 by jorteixe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/Bureaucrat.hpp"
+
+int main(void)
+{
+	try
+	{
+		Bureaucrat toni("toni", 151);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		Bureaucrat toni("toni", 0);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	try
+	{
+		Bureaucrat toni("toni", 1);
+		std::cout << toni << "\n";
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+}

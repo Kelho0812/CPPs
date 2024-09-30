140e6840f8e9fd6ec50c17c5779c678583a0a246
Jorge Costa <Kelho88@pm.me>
2024-09-30 10:06:59 +0100

    Add cpp07/ex00/  swap, min & max implementation

--- cpp07/ex00/includes/main.hpp
+++ cpp07/ex00/includes/main.hpp
@@ -10,3 +10,30 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <iostream>
+
+template <class T> T max(T a, T b)
+{
+	if (a > b)
+		return (a);
+	else
+		return (b);
+}
+
+template <class T> T min(T a, T b)
+{
+	if (a < b)
+		return (a);
+	else
+		return (b);
+}
+
+template <class T> void swap(T &a, T &b)
+{
+	T c;
+	c = a;
+	a = b;
+	b = c;
+}
